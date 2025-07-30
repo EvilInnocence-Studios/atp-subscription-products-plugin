0731fabecf1ac95277ee02dc55890269ff3a1cc2
Andrew Wittrock <andy@wittrock.us>
2025-07-29 23:44:42 -0500

    Change priority of subscription only product edit controls

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import { IModule } from "@core/lib/module";
-import { plugins as storePlugins } from "@store/lib/plugin/slots";
+import { storePlugins } from "@store/lib/plugin/slots";
 import { subscriptionOnlyToggle } from "./lib/plugins/productEditor";
 import { subscribeButton } from "./lib/plugins/subscribeButton";
 
@@ -10,7 +10,7 @@ export const module:IModule = {
 // -- Register plugins -- //
 
 // Adds a button to the product editor that allows toggling the subscription-only status of a product.
-storePlugins.product.editor.details.register(100, subscriptionOnlyToggle);
+storePlugins.product.editor.details.register(900, subscriptionOnlyToggle);
 
 // Adds an add to cart button overrides for subscription products.
 storePlugins.cart.addButton.register(subscribeButton);
